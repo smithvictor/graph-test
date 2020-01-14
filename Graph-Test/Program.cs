fa08e5685eb2eec3ae5d5678bab21320938967af
Victor Smith <smith@sunu.io>
2020-01-14 12:29:50 -0600

    Add project files.

---
+++ Graph-Test/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Graph_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
